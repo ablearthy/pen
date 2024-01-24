55fd97a7c331a76a9b931dea38afe8d971836687
Timur Aminev <ablearthy@gmail.com>
2024-01-24 21:23:51 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -4,11 +4,9 @@
   inputs = {
     # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    vscoq.url = "github:ablearthy/vscoq";
-    vscoq.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, vscoq, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -18,7 +16,7 @@
           coqPackages.coq
           coqPackages.mathcomp-ssreflect
 
-          vscoq.packages.${system}.vscoq-language-server
+          coqPackages.coq-lsp
         ];
       in {
         devShell = pkgs.mkShell ({
