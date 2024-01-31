f630a4c2c6804088f6956e2c61b5928bf79a6d8c
Timur Aminev <ablearthy@gmail.com>
2024-01-31 13:07:15 +0300

    induction.v: Add solution for sum

--- induction.v
+++ induction.v
@@ -31,3 +31,49 @@ Proof.
 Admitted.
   
   
+(* Sum *)
+
+Fixpoint sum (l : seq nat) : nat :=
+  match l with
+  | [::] => 0
+  | x :: xs => x + sum xs
+  end.
+
+Fixpoint sum_tail' (acc : nat) (l : seq nat) : nat :=
+  match l with
+  | [::] => acc
+  | x :: xs => sum_tail' (acc + x) xs
+  end.
+    
+Definition sum_tail := sum_tail' 0.
+
+Theorem sum_tail_correct (l : seq nat) : sum_tail l = sum l.
+Proof.
+  elim: l=>//=.
+  move=> x xs.
+  rewrite /sum_tail=>//=.
+  rewrite add0n.
+Abort.
+
+Lemma sum_tail'_sum (l : seq nat) (n acc : nat) : sum_tail' (acc + n) l = n + sum_tail' acc l.
+Proof.
+  elim: l acc=>//=.
+  by move=> acc ; rewrite addnC.
+  move=> x xs H acc.
+  by rewrite -addnA [n+x]addnC addnA (H (acc + x)).
+Qed.
+
+Theorem sum_tail'_correct (l : seq nat) (acc : nat) : sum_tail' acc l = acc + sum l.
+Proof.
+  elim: l=>//=.
+  by rewrite addn0.
+  move=> x xs.
+  rewrite [x + _]addnC addnA.
+  move=><-.
+  by rewrite [sum_tail' _ _ + _]addnC sum_tail'_sum.
+Qed.
+
+Theorem sum_tail_correct (l : seq nat) : sum_tail l = sum l.
+Proof.
+  by rewrite -[sum _]add0n /sum_tail sum_tail'_correct.
+Qed.
